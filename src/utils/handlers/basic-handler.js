42759874dbf4c0d9b4daa98d8cc9b4c2bac2c200
mitrofazxc <mitrofanzxc@gmail.com>
2024-10-15 02:06:11 +0300

    feat: zip-handler

--- src/utils/handlers/basic-handler.js
+++ src/utils/handlers/basic-handler.js
@@ -1,1 +1,11 @@
-export const basicHandler = async () => {};
+import { showErrorMessage } from "../show-error-message.js";
+
+import { ErrorMessage } from "../../constants/index.js";
+
+export const basicHandler = async (operationType, args, currentWorkingDirectory) => {
+    try {
+        const tempOperationType = operationType?.toLowerCase();
+    } catch {
+        showErrorMessage(ErrorMessage.BASIC_OPERATION);
+    }
+};
--- src/utils/navigation/navigate-up.js
+++ src/utils/navigation/navigate-up.js
@@ -1,6 +1,8 @@
 import { dirname } from "node:path";
 import { cwd, chdir } from "node:process";
 
+import { showErrorMessage } from "../show-error-message.js";
+
 import { ErrorMessage } from "../../constants/index.js";
 
 export const navigateUp = () => {
@@ -10,6 +12,6 @@ export const navigateUp = () => {
 
         chdir(parentWorkingDirectory);
     } catch {
-        printErrorText(ErrorMessage.NAVIGATION_UP);
+        showErrorMessage(ErrorMessage.NAVIGATION_UP);
     }
 };
